dfdec96f856638e845cb01460eb81a17a036dda3
kiran.goud.merugu <kiran.goud.merugu@accenture.com>
2017-09-26 09:19:28 +0000

    ddd

--- cookbooks/sap/recipes/default.rb
+++ cookbooks/sap/recipes/default.rb
@@ -5,4 +5,37 @@
 # Copyright 2017, YOUR_COMPANY_NAME
 #
 # All rights reserved - Do Not Redistribute
+app_user = "#{node[:app_user]}"
+app_user_group =  "#{node[:app_user_group]}"
+path =  "#{node[:directory]}"
+
+user app_user do
+  uid '1234'
+  gid '1234'
+  home '/home/sap'
+  shell '/bin/bash'
+  password 'sap123'
+end
+
+directory path do
+  owner  app_user
+  group  app_user_group
+  mode '0755'
+  action :create
+end
+
+mount "/var/mnt" do
+  device xvdf
+  fstype 'ext4'
+  action :mount
+end
+
+filesystem "label" do
+  fstype "ext3"
+  device "/dev/xvdf"
+  mount "/var/mnt"
+  action [:create, :enable, :mount]
+end
+
+
 #
