97ce1e5c9ff6b09bf748595586f8dd4b0e3a34cf
kiran.goud.merugu <kiran.goud.merugu@accenture.com>
2017-10-17 11:55:57 +0000

    ddd

--- cookbooks/test/recipes/default.rb
+++ cookbooks/test/recipes/default.rb
@@ -6,3 +6,31 @@
 #
 # All rights reserved - Do Not Redistribute
 #
+directory "/mnt/my-backup" do
+  owner 'root'
+  group 'root'
+  recursive true
+  action :create
+  mode '0755'
+end
+
+mount ' /mnt/my-back-up' do
+  device '/dev/xvda1'
+  fstype 'ext4'
+end
+
+bash "copy back up" do
+  code <<-EOL
+  cp -a /etc/* /mnt/my-backup
+  EOL
+end
+
+execute "apt update" do
+command "apt-get update -y"
+action :run
+end
+
+execute "apt dist-upgrade" do
+command "apt-get dist-upgrade -y"
+action :run
+end
