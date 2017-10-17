4906a0a6118ac9998a7e82c75e8af969ab0601b1
kiran.goud.merugu <kiran.goud.merugu@accenture.com>
2017-10-17 12:21:35 +0000

    ddd

--- cookbooks/ubuntu_patch/recipes/default.rb
+++ cookbooks/ubuntu_patch/recipes/default.rb
@@ -1,19 +1,37 @@
-#
-# Cookbook Name:: ubuntu_patch
-# Recipe:: default
-#
-# Copyright 2017, YOUR_COMPANY_NAME
-#
-# All rights reserved - Do Not Redistribute
-#
+directory "/mnt/my-backup" do
+  owner 'root'
+  group 'root'
+  recursive true
+  action :create
+  mode '0755'
+
+end
+execute "copy_core" do
+command "cp -a /etc/* /mnt/my-back-up"
+user "root"
+end
 
 execute "apt update" do
- command "apt-get update"
- action :run
+command "apt-get update -y"
+action :run
 end
+
 execute "apt dist-upgrade" do
- command "apt-get dist-upgrade -y"
- action :run
+command "apt-get dist-upgrade -y"
+action :run
+end
+
+execute "apt update-manager-core" do
+command "apt-get update-manager-core"
+action :run
+end
+
+execute "do-release-upgrade" do
+command "do-release-upgrade "
+action :run
+end
+execute "do-release-upgrade" do
+command "do-release-upgrade -d"
 end
 
 
